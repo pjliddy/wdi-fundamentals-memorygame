808e25ae82371b7fd25a08f931e4b1dc91219d1b
Patrick Liddy <pjliddy@gmail.com>
2017-03-14 13:35:49 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,4 +1,30 @@
-var cards = [ "queen", "queen", "king", "king" ];
+
+var cards = [ 
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
+	},
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	}
+];
+
+
+
+
 var cardsInPlay = [ ];
 
 var checkForMatch = function() {
@@ -12,9 +38,9 @@ var checkForMatch = function() {
 }
 
 var flipCard = function(cardId) {
-	cardsInPlay.push(cards[cardId]);
-	console.log("User flipped " + cards[cardId]);
-
+	cardsInPlay.push(cards[cardId].rank);
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
 	checkForMatch();
 	return;
 }
